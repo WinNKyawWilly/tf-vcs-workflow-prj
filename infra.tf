5a18d8eb2417a986d4e143e95e81b53332aaba17
WinNKyawWilly <winnaingkyaw_win@cmu.ac.th>
2024-09-29 10:25:49 +0700

    Commented Codes

--- infra.tf
+++ infra.tf
@@ -1,19 +1,19 @@
-resource "aws_vpc" "rancher_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  tags = {
-    Name = "${var.prefix}-rancher-vpc"
-  }
-}
+# resource "aws_vpc" "rancher_vpc" {
+#   cidr_block           = "10.0.0.0/16"
+#   enable_dns_hostnames = true
+#   tags = {
+#     Name = "${var.prefix}-rancher-vpc"
+#   }
+# }
 
-resource "aws_subnet" "rancher_subnet" {
-  vpc_id = aws_vpc.rancher_vpc.id
+# resource "aws_subnet" "rancher_subnet" {
+#   vpc_id = aws_vpc.rancher_vpc.id
 
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = var.aws_zone
+#   cidr_block        = "10.0.1.0/24"
+#   availability_zone = var.aws_zone
 
-  tags = {
-    Name = "${var.prefix}-ranher-subnet"
-  }
-}
+#   tags = {
+#     Name = "${var.prefix}-ranher-subnet"
+#   }
+# }
 
--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
-variable "prefix" {
-  type        = string
-  description = "Prefix added to names of all resources"
-  default     = "quickstart"
-}
+# variable "prefix" {
+#   type        = string
+#   description = "Prefix added to names of all resources"
+#   default     = "quickstart"
+# }
 
 variable "aws_region" {
   type    = string
   default = "ap-southeast-1"
 }
 
-variable "aws_zone" {
-  type        = string
-  description = "AWS zone used for all resources"
-  default     = "ap-southeast-1a"
-}
+# variable "aws_zone" {
+#   type        = string
+#   description = "AWS zone used for all resources"
+#   default     = "ap-southeast-1a"
+# }
